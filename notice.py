624f12625d91a7af8a80b75e6d4bd5ad3119ae0a
amo0721 <46621867+amo0721@users.noreply.github.com>
2019-06-19 16:28:24 +0900

    Update notice.py

--- notice.py
+++ notice.py
@@ -73,7 +73,7 @@ async def on_message(message):
         
     if id in afk:
         embed = discord.Embed(title="잠수 상태가 해제되었어요!", description=str(message.author.name) + "님께서 잠수 상태가 해제되셨습니다.", color=0x0000ff)
-        await client.send_message(message.channel, embed=embed)
+        await app.send_message(message.channel, embed=embed)
         afk.remove(id)
 
     if message.content.startswith('/잠수'):
@@ -87,20 +87,20 @@ async def on_message(message):
         if learn == '':
             embed = discord.Embed(title="잠수 시작!", color=0x00ff00)
             embed.add_field(name=(message.author.name) + "님의 잠수 상태가 시작되었습니다!!!", value="잠수 시작 시간 : " + str(a) + "년" + str(b) + "월" + str(c) + "일" + str(d) + "시" + str(e) + "분")
-            await client.send_message(message.channel, embed=embed)
+            await app.send_message(message.channel, embed=embed)
         else:
             embed = discord.Embed(title="잠수 시작!", color=0x00ff00)
             embed.add_field(name=(message.author.name) + "님의 잠수 상태가 시작되었습니다!!!", value="잠수 시작 시간 : " + str(a) + "년" + str(b) + "월" + str(c) + "일" + str(d) + "시" + str(e) + "분")
             embed.add_field(name="사유", value=learn)
-            await client.send_message(message.channel, embed=embed)
+            await app.send_message(message.channel, embed=embed)
 
     if message.content.startswith('/기능 종료 잠수'):
         embed = discord.Embed(title="잠수 모듈 종료", color=0xff0000)
         embed.add_field(name="잠수 모듈 종료", value="요청자 : " + str(message.author.name))
-        await client.send_message(client.get_channel(Setting.err_loging_channel), embed=embed)
+        await app.send_message(client.get_channel(Setting.err_loging_channel), embed=embed)
         quit()
 
                         
 
-
-app.run("NTQ3NDEzOTcxMjAwOTAxMTIw.XOI5Wg.1pKHZRbctSJ8SRrpYCxwna5ASxA")
+access_token = os.environ["BOT_TOEKN"]
+app.run(access_token)
