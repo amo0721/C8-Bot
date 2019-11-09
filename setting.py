5aa074595b9cdcb7d7e7788bf8f6bc8169ff4add
amo0721 <46621867+amo0721@users.noreply.github.com>
2019-11-09 10:24:18 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -34,11 +34,11 @@ import datetime
 
 class Settings:
     def __init__(self):
-        self.owner_id = "550876882246303774"
+        self.owner_id = "467625748631519244"
         self.log_file = "msg_log.rtl"
         self.actvity_log_file = "actvity_log.rtl"
-        self.err_loging_channel = "577119036832481280"
-        self.bot_id = "547413971200901120", "415401437246521344", "387548561816027138", "484395597508509697"
-        self.notice_channel = "591961466794409996"
-        self.welcome_channel = "576592307978108929"
-        self.ban_user_id = "457537955188178955", "467625748631519244"
+        self.err_loging_channel = "642533660347596801"
+        self.bot_id = "159985870458322944"
+        self.notice_channel = "642534157234470912"
+        self.welcome_channel = "599012430449737758"
+        self.ban_user_id = "0"
