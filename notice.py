3591a9b8df63a40feb6e80a526ec3ca9d471d7cd
amo0721 <46621867+amo0721@users.noreply.github.com>
2020-01-06 10:39:36 +0900

    Update notice.py

--- notice.py
+++ notice.py
@@ -10,14 +10,14 @@ async def on_ready():
     
 @app.event
 async def on_member_join(member):
-    embed = discord.Embed(title="환영해요!", description = None, color=0x0000ff)
-    embed.add_field(name="유저 닉네임 : " + (member.name), value="유저 아이디 : " + (member.id))
+    embed = discord.Embed(title="환영해요! Welcome!", description = None, color=0x0000ff)
+    embed.add_field(name="유저 닉네임(User Nickname) : " + (member.name), value="유저 아이디(User ID) : " + (member.id))
     await app.send_message(app.get_channel(Setting.welcome_channel), embed=embed)
 
 @app.event
 async def on_member_remove(member):
-    embed = discord.Embed(title="잘가요...ㅜㅜ", color=0x00ff00)
-    embed.add_field(name="유저 닉네임 : " + (member.name), value="유저 아이디 : " + (member.id))
+    embed = discord.Embed(title="잘가요...ㅜㅜ Good bye... TT", color=0x00ff00)
+    embed.add_field(name="유저 닉네임(User Nickname) : " + (member.name), value="유저 아이디(User ID) : " + (member.id))
     await app.send_message(app.get_channel(Setting.welcome_channel), embed=embed)
 
 
@@ -30,7 +30,7 @@ async def on_message(message):
                                return None
                 
                 if id in afk:
-                    embed = discord.Embed(title="잠수 상태가 해제되었어요!", description=str(message.author.name) + "님께서 잠수 상태가 해제되셨습니다.", color=0x0000ff)
+                    embed = discord.Embed(title="잠수 상태가 해제되었어요! Your afk is unlocked!", description=str(message.author.name) + "님께서 잠수 상태가 해제되셨습니다.", color=0x0000ff)
                     await app.send_message(message.channel, embed=embed)
                     afk.remove(id)
         
@@ -40,17 +40,17 @@ async def on_message(message):
                     if message.author.server_permissions.administrator:
                         learn = message.content.replace('/게임', "")
                         await app.change_presence(game=discord.Game(name=learn))
-                        await app.send_message(message.channel, "봇의 게임을 변경하였습니다.")
+                        await app.send_message(message.channel, "봇의 게임을 변경하였습니다. I change the bot's game.")
 
                 if message.content.startswith("/온라인"):
-                    embed=discord.Embed(title="Jenon BOT 온라인 상황", description=None, color=0x00ff00)
-                    embed.add_field(name="I'm online!", value="이 메세지가 발신되지 않으면 Offline 입니다.")
-                    embed.add_field(name="요청자", value="<@" + str(message.author.name) + ">")
+                    embed=discord.Embed(title="Jenon BOT 온라인 상황 Jenon BOT's online message", description=None, color=0x00ff00)
+                    embed.add_field(name="I'm online!", value="이 메세지가 발신되지 않으면 Offline 입니다. If the message isn't send, the Bot is offline.")
+                    embed.add_field(name="요청자 Insert user", value="<@" + str(message.author.name) + ">")
                     await app.send_message(message.channel, embed=embed)  
                                           
                 if message.content.startswith("/상태"):
                     if message.author.id == Setting.owner_id:
-                        embed = discord.Embed(title="Jenon Bot 서버 상태", color=0x00ff00)
+                        embed = discord.Embed(title="Jenon Bot 서버 상태 Jenon Bot Server", color=0x00ff00)
                         embed.add_field(name="Owner id", value=Setting.owner_id, inline=True)
                         embed.add_field(name="classic log channel id", value=Setting.err_loging_channel, inline=True)
                         embed.add_field(name="Bot Notice Channel", value=Setting.notice_channel, inline=True)
@@ -65,36 +65,36 @@ async def on_message(message):
                     d = datetime.datetime.today().hour
                     e = datetime.datetime.today().minute
                     f = datetime.datetime.today().second
-                    embed=discord.Embed(title='`Jenon Bot 도움말 목록`', description=None, color=0xb2ebf4)
-                    embed.add_field(name='`/온라인`', value='봇이 온라인인지 확인할 수 있습니다.', inline=False)
-                    embed.add_field(name='`/도움말`', value='Jenon Bot 도움말을 출력합니다.', inline=False)
-                    embed.add_field(name='`/패치노트`', value="봇의 최근 업데이트 내용을 출력합니다.", inline=False)
-                    embed.add_field(name='`/서버정보`', value="서버 정보를 출력합니다.", inline=False)
-                    embed.add_field(name='`/잠수 [사유]`', value="잠수 상태에 돌입합니다. 사유도 넣을 수 있습니다.", inline=False)
-                    embed.add_field(name='`/관리자 소개`', value="OverWatch discord Server | 서버 관리자들을 소개합니다!!!")
-                    embed.set_footer(text="관리자 명령어는 '/관리자 도움말' 입력! | " + str(a) + "년 " + str(b) + "월 " + str(c) + "일 " + str(d) + "시 " + str(e) + "분 " + str(f) + "초")
+                    embed=discord.Embed(title='`Jenon Bot 도움말 목록 Jenon Bot helper(command list)`', description=None, color=0xb2ebf4)
+                    embed.add_field(name='`/온라인`', value='봇이 온라인인지 확인할 수 있습니다. You can check the bot is online.', inline=False)
+                    embed.add_field(name='`/도움말`', value='Jenon Bot 도움말을 출력합니다. The bot is print command list.', inline=False)
+                    embed.add_field(name='`/패치노트`', value="봇의 최근 업데이트 내용을 출력합니다. The bot is print the lastest update content.", inline=False)
+                    embed.add_field(name='`/서버정보`', value="서버 정보를 출력합니다. The bot is print this server information(But, you can't use this command in individual message.", inline=False)
+                    embed.add_field(name='`/잠수 [사유]`', value="잠수 상태에 돌입합니다. 사유도 넣을 수 있습니다. Your activity is in afk. You can insert reason, too.", inline=False)
+                    embed.add_field(name='`/관리자 소개`', value="OverWatch discord Server | 서버 관리자들을 소개합니다!!! Introduce this server's admin.")
+                    embed.set_footer(text="관리자 명령어는 '/관리자 도움말' 입력! If you want to insert admin's command, insert '/관리자 도움말'.| " + str(a) + "년 " + str(b) + "월 " + str(c) + "일 " + str(d) + "시 " + str(e) + "분 " + str(f) + "초")
                     await app.send_message(message.channel, embed=embed)
 
                 if message.content.startswith('/관리자 도움말'):
-                    embed=discord.Embed(title='`Jenon Bot 관리자 도움말 목록`', color=0x00ff00)
-                    embed.add_field(name='/공지 [내용]', value="공지 채널에 내용을 공지로 전송합니다.")
-                    embed.add_field(name='/종료', value='봇을 종료합니다.')
-                    embed.add_field(name='/게임 [게임내용]', value='플레이중 상태를 [게임내용]으로 변경합니다.')
-                    embed.add_field(name='/상태', value='Setting.py 및 서버 상태를 알려줍니다.')
+                    embed=discord.Embed(title='`Jenon Bot 관리자 도움말 목록 Jenon Bot Admin command list`', color=0x00ff00)
+                    embed.add_field(name='/공지 [내용]', value="공지 채널에 내용을 공지로 전송합니다. Send content to channel what name is notice.")
+                    embed.add_field(name='/종료', value='봇을 종료합니다. Turn off the bot.')
+                    embed.add_field(name='/게임 [게임내용]', value='플레이중 상태를 [게임내용]으로 변경합니다. Change playing activity to [게임내용].')
+                    embed.add_field(name='/상태', value='Setting.py 및 서버 상태를 알려줍니다. Setting.py and server state.')
                     await app.send_message(message.channel, embed=embed)
 
                 if message.content.startswith("/서버정보"):
-                    embed = discord.Embed(title="\"%s\" 서버정보!" % (message.server.name), description=None, color=0X00ff00)
-                    embed.add_field(name="서버 소유자", value="<@%s>" % message.server.owner.id, inline=False)
-                    embed.add_field(name="서버 생성일", value="%s (UTC)" % (message.server.created_at), inline=False)
-                    embed.add_field(name="서버 보안등급", value=message.server.verification_level, inline=False)
-                    embed.add_field(name="서버 위치", value=message.server.region, inline=False)
-                    embed.add_field(name="서버 잠수채널", value="%s (%s분 이상 잠수이면 이동됨)" % (message.server.afk_channel, message.server.afk_timeout/60), inline=False)
+                    embed = discord.Embed(title="\"%s\" 서버정보! Server information." % (message.server.name), description=None, color=0X00ff00)
+                    embed.add_field(name="서버 소유자 Server Owner", value="<@%s>" % message.server.owner.id, inline=False)
+                    embed.add_field(name="서버 생성일 Made server created at", value="%s (UTC)" % (message.server.created_at), inline=False)
+                    embed.add_field(name="서버 보안등급 server verification level", value=message.server.verification_level, inline=False)
+                    embed.add_field(name="서버 위치 server region", value=message.server.region, inline=False)
+                    embed.add_field(name="서버 잠수채널 server afk channel", value="%s (%s분 이상 잠수이면 이동됨)" % (message.server.afk_channel, message.server.afk_timeout/60), inline=False)
                     embed.set_thumbnail(url=message.server.icon_url)
                     await app.send_message(message.channel, embed=embed)
 
                 if message.content.startswith('/종료'):
-                    embed = discord.Embed(title="메인 모듈 종료", color=0xff0000)
+                    embed = discord.Embed(title="메인 모듈 종료 Turn off Main module.", color=0xff0000)
                     embed.add_field(name="The main module is change to offline.", value="요청자 : " + str(message.author.name))
                     await app.send_message(app.get_channel(Setting.err_loging_channel), embed=embed)
                     quit()
@@ -142,8 +142,8 @@ async def on_message(message):
                         await app.send_message(message.channel, embed=embed)
                 if message.content.startswith('/패치노트'):
                     embed = discord.Embed(title="업데이트 내역", color=0x00ff00)
-                    embed.add_field(name="●초대링크 생성/초대 명령어 수정", value="Jenon Bot의 초대 링크와 지원 서버의 링크를 추가하였고, 초대 명령어 부분을 전면 수정하였습니다.")
-                    embed.add_field(name="●긴급수정:봇 업데이트", value="20191201 Update")
+                    embed.add_field(name="●일부 명령어 영어화(베타 버전)", value="Jenon Bot의 일부 명령어에 영어를 추가하였습니다. 추가되지 않은 부분은 추후 추가 예정입니다.")
+                    embed.add_field(name="●긴급수정:봇 업데이트", value="20200106 Update")
                     await app.send_message(message.channel, embed=embed)
 
                 if "/" in message.content:
